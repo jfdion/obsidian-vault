a6458a11a1834c9213343c488213b49fa4928580
Jean-François Dion <jfdion@csfoy.ca>
2024-05-08 10:50:03 -0400

    vault backup: 2024-05-08 10:50:03 - 4 files from

--- extensions/plugins/Templater/obsidian-extender/vite.config.js
+++ extensions/plugins/Templater/obsidian-extender/vite.config.js
@@ -1,39 +1,13 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
-const config = {
-  tplExt: {
-    entry: resolve(__dirname, "./src/templateExtender.ts"),
-    filename: "tplExt.js"
-  },
-  sessionExt: {
-    entry: resolve(__dirname, "./src/sessionExtender.ts"),
-    filename: "sessionExt.js"
-  },
-  seanceExt: {
-    entry: resolve(__dirname, "./src/seanceExtender.ts"),
-    filename: "seanceExt.js"
-  },
-  configSession: {
-    entry: resolve(__dirname, "./src/configSession.ts"),
-    filename: "configSession.js"
-  }
-}
-
-const currentConfig = config[process.env.LIB_NAME];
-
-if (currentConfig === undefined) {
-  throw new Error("LIB_NAME is not defined or is not valid")
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: { 
-    outdir: "/dist",
-    lib: { 
-      ...currentConfig, 
-      formats: ['cjs', 'es'] 
+  build: {
+    lib: {
+      entry: glob.sync(resolve(__dirname, 'src/*.ts')),
+      formats: ["es", "cjs"],
+      fileName: (format, name) => `${name}.${format === 'es' ? 'mjs' : 'cjs'}`,
     },
-    emptyOutDir: false
   }
 })
