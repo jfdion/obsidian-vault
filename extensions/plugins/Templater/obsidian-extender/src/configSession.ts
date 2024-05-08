53614b65304d81f702ece1aa6e6639ab32a06833
Jean-François Dion <jfdion@csfoy.ca>
2024-05-08 15:17:03 -0400

    vault backup: 2024-05-08 15:17:02 - 1 files from

--- extensions/plugins/Templater/obsidian-extender/src/configSession.ts
+++ extensions/plugins/Templater/obsidian-extender/src/configSession.ts
@@ -1,4 +1,6 @@
-expor type Cours = { short: string, number: string, long: string }
+import html from "./html"
+
+export type Cours = { short: string, number: string, long: string }
 
 const PO2: Cours = { short: "PO2", number: "420-W30-SF", long: "Programmation orientée objet II" }
 const CS: Cours = { short: "CS", number: "420-W53-SF", long: "Cybersécurité" }
@@ -8,15 +10,40 @@ const SW: Cours = { short: "SW", number: "420-W41-SF", long: "Services Web" }
 const ST: Cours = { short: "ST", number: "420-W70-W71-SF", long: "Stages" }
 
 
-
 const semester: Record<string, Cours[]> = {
-    "A24": [PO2, CS, IT],
-    "H24": [BD1, SW, ST]
+    "A24": [CS, IT, PO2].sort(sortCourse),
+    "H24": [BD1, SW, ST].sort(sortCourse)
 }
 
+function sortCourse(a: Cours, b: Cours) {
+    if (a.number < b.number) {
+        return -1;
+    }
+    if (a.number > b.number) {
+        return 1;
+    }
+    return 0
+}
+
+function courseToChip(cours: Cours) {
+    return html.chip(`cours-${index}`, cours.short);
+}
+
+function courseIndex(session: string): (cours: Cours) => number {
+    return (cours: Cours): number => {
+        return semester[session].findIndex(c => c.long === cours.long
+            || cours.short === cours.short
+            || cours.number === cours.number
+        )
+    }
+
+}
+
+
 const configSession = {
-    chip,
-    highlight
+    (session: string) => {
+    return
+}
 }
 
 export default configSession
