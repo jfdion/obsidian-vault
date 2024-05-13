3919b2ffbad68e9faa4519f497ff00fdc08507c7
Jean-François Dion <jfdion@csfoy.ca>
2024-05-13 15:34:27 -0400

    vault backup: 2024-05-13 15:34:27 - 1 files from

--- extensions/plugins/Templater/obsidian-extender/src/seance.ts
+++ extensions/plugins/Templater/obsidian-extender/src/seance.ts
@@ -1,4 +1,32 @@
+import { Semester } from "./configSession"
+import { Course, Schedule } from "./course"
 import templaterExtender from "./templaterExtender"
+
+
+enum SeanceState {
+    READY = 'ready',
+    TODO = 'todo',
+    TO_REVIEW = 'to-review',
+    DONE = 'done',
+    NA = 'na'
+}
+
+type Seance = {
+    filename: string,
+    cours: Course,
+    session: Semester,
+    date: Date,
+    weekNumber: number,
+    schedule: Schedule,
+    state: SeanceState
+    MOC: string
+}
+
+function NewSeance(date: Date, semesterCode: string): Seance {
+    return {
+        filename: 
+    }
+}
 
 function nomCours(tp: any, depth = 2) {
     const folderName = templaterExtender.getParentFolder(tp, depth)
