49a2ee1bcd5f058a950dbd0c3f133d37b261bbf9
Jean-François Dion <jfdion@csfoy.ca>
2024-05-13 15:44:52 -0400

    vault backup: 2024-05-13 15:44:52 - 1 files from

--- extensions/plugins/Templater/obsidian-extender/src/functional.ts
+++ extensions/plugins/Templater/obsidian-extender/src/functional.ts
@@ -1,6 +1,10 @@
-const pipe = <T>(...fns: Array<(arg: T) => T>) => (value: T) => fns.reduce((acc, fn) => fn(acc), value);
+
+type AnyFunc = (...args: any) => any;
+type PipeArgs<F extends AnyFunc[], Acc extends AnyFunc[] = []> = F extends [
+    (...args: infer A) => infer 
+] ? [...access, (...args: A) => B]
 
 
-export function pipe<T, U>(...fns: Array<(arg: T)> => T): (value: T) => U {
-
+export function pipe<T, U>(...fns: Array<PipableFn<T, U>>): (value: T) => T | U {
+    return (value: T) => fns.reduce((acc, fn) => fn(acc), value);
 }
